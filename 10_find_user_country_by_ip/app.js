d4d18e499a9ebb34a99dc15bf60bbf6d5f5bc67d
FunkyPotato7 <funkypotato0@gmail.com>
2023-11-06 12:03:52 +0200

    added #10 task

---
+++ 10_find_user_country_by_ip/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import fs from 'fs';
+import { IPTools } from 'ip2location-nodejs';
+
+const app = express();
+
+const tools = new IPTools();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/getCountryInfo', (req, res) => {
+    const { ip } = req.body;
+    const location = tools.ipV4ToDecimal(ip);
+
+    fs.readFile('IP2LOCATION-LITE-DB1.CSV', (err, data) => {
+         const parsedData = data.toString().split('\r\n').map(el => el.replace(/"/g, '').split(','));
+
+         for (const arr of parsedData) {
+             if (location >= Number(arr[0]) && location <= Number(arr[1])) {
+                 res.status(200).json({
+                     range: `${arr[0]}-${arr[1]}`,
+                     country: arr[3]
+                 });
+             }
+         }
+     });
+});
+
+app.listen(5000, () => {
+    console.log('Server listen 5000');
+});
