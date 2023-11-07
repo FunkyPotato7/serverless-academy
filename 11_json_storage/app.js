d8d99fba0433b90f208b83979da856ec221c3285
FunkyPotato7 <funkypotato0@gmail.com>
2023-11-07 21:37:06 +0200

    added error handlers

--- 11_json_storage/app.js
+++ 11_json_storage/app.js
@@ -1,38 +1,48 @@
 import express from 'express';
 import fs from 'fs';
 
+import APIError from './errors/APIError.js';
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/:json_path', async (req, res) => {
+app.get('/:json_path', async (req, res, next) => {
     const { json_path } = req.params;
 
-    await fs.readFile(`./files/${json_path}.json`, (err, data) => {
-        if (data) {
+    fs.readFile(`./files/${json_path}.json`, (err, data) => {
+        try {
+            if (err) {
+                throw new APIError(`File ${json_path} not exist`, 404);
+            }
             const parsedData = JSON.parse(data);
             res.status(200).json(parsedData);
-        } else {
-            res.status(404).json(`File ${json_path} not exist`);
+        } catch (e) {
+            next(e);
         }
     });
 });
 
-app.put('/:json_path', (req, res) => {
+app.put('/:json_path', (req, res, next) => {
     const { body, params } = req;
+
     const jsonData = JSON.stringify(body);
 
-    fs.writeFile(`./files/${params.json_path}.json`, jsonData, (err) => {
-        if (err) {
-            console.log(err);
+    fs.writeFile(`./filejs/${params.json_path}.json`, jsonData, (err) => {
+        try {
+            if (err) {
+                throw new APIError(err.message, 404);
+            }
+            res.sendStatus(200);
+        } catch (e) {
+            next(e);
         }
     });
-    res.sendStatus(200);
 });
 
 app.use((err, req, res, next) => {
-    res.status(err.status || 500).json(err.message);
+    res.status(err.status || 500).json(err);
 });
 
 app.listen(5000, () => {
